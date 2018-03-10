5b81d4a5c7b5bfb0699f0f0be433840c833f83cc
Yukino Hayakawa <tennencoll@gmail.com>
2018-03-10 23:05:19 +0800

    add profile app for attaching information to users

--- Attendance/Attendance/settings.py
+++ Attendance/Attendance/settings.py
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'profile',
     'session',
-    # Add your apps here to enable them
+    'absence_form',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
