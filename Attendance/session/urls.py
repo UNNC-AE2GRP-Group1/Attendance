5ae4af08396e521f06883d6fad2a5acc0033b59b
MarvinZeng <772400725@qq.com>
2018-03-22 18:58:05 +0800

    mainly change the code in Module folder

---
+++ Attendance/session/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url('^sessions/$', views.session_index),
+    url('^modules/$', views.module_index),
+]
--- Attendance/session/views.py
+++ Attendance/session/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def module_index(request):
+    return render(request, 'module/index.html')
+
+def session_index(request):
+    return render(request, 'session/index.html')
