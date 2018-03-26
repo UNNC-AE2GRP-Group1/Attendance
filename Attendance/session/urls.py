d6289f3e75bc48bb9565f6e091be8f720ffadf4f
Yukino Hayakawa <tennencoll@gmail.com>
2018-03-26 16:32:40 +0800

    add navbar items; remove BOM chars

--- Attendance/session/urls.py
+++ Attendance/session/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^sessions/$', views.session_index),
+    url(r'^sessions/$', views.session_overview, name='session-overview'),
     url(r'^sessions/(?P<session_pk>\w+)/attendance$', views.session_taking_attendance),
-    url(r'^modules/$', views.module_index),
+    url(r'^modules/$', views.module_index, name='module-index'),
     url(r'^modules/(?P<module_pk>\w+)/attendance$', views.module_attendance_history),
 ]
