6a1420419521e8cdd3525b216471ab506cf8e003
Yukino Hayakawa <tennencoll@gmail.com>
2018-03-28 20:31:15 +0800

    add name extraction policy based on detecting UPPERCASES

--- Attendance/session/static/module/scripts/student_import.js
+++ Attendance/session/static/module/scripts/student_import.js
@@ -12,10 +12,9 @@ $("#btn-preview-csv").click(function () {
                 console.log("Parsing complete:", results, file);
 
                 var namePolicyChoice = $('input[name=name-policies]:checked').val();
-                var skipHeader = $('#has-header').checked;
+                var skipHeader = $('#skip-header').is(":checked");
 
                 var studentCount = results.data.length;
-                var list = $('#student-list');
 
                 var colStudentId = $('#col-studentid').val();
                 var colFirstname = $('#col-firstname').val();
@@ -29,30 +28,56 @@ $("#btn-preview-csv").click(function () {
                         'lastname': row[colLastname]
                     };
                 };
-                var namePolicy = function () { }; // todo
+                var namePolicySingle = function (row) {
+                    if (!row[colFullname]) return {};
+                    var parts = row[colFullname].split(/[\s,]+/);
+                    var lastnames = [];
+                    var firstnames = [];
+                    for (var i = 0; i < parts.length; i++) {
+                        if (parts[i] === parts[i].toUpperCase())
+                            lastnames.push(parts[i]);
+                        else
+                            firstnames.push(parts[i]);
+                    }
+                    return {
+                        'firstname': firstnames.join(" "),
+                        'lastname': lastnames.join(" ")
+                    };
+                };
+                var namePolicy = function () { };
 
                 if (namePolicyChoice === 'separate') {
                     namePolicy = namePolicySeparate;
+                } else if(namePolicyChoice === 'single') {
+                    namePolicy = namePolicySingle;
                 }
 
                 var data = [];
+
+                var toTitleCase = function(str) {
+                    return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1);
+                }
 
                 for (var i = skipHeader ? 1 : 0; i < studentCount; i++) {
                     var studentId = results.data[i][colStudentId];
                     if (!studentId) continue;
 
-                    ++numValidStudents;
-
                     var student = namePolicy(results.data[i]);
+                    if (!student['firstname'] || !student['lastname']) continue;
+
                     student['studentid'] = studentId;
+                    student['firstname'] = toTitleCase(student['firstname']);
+                    student['lastname'] = toTitleCase(student['lastname']);
 
                     data.push({ id: i, values: student });
+
+                    ++numValidStudents;
                 }
 
                 $('#preview-message').text(`Found ${numValidStudents} records`);
 
                 studentEditableGrid.load({ "metadata": metadata, "data": data });
-                studentEditableGrid.renderGrid("student-list", "table");
+                studentEditableGrid.renderGrid("student-list-preview", "table");
             }
         },
         before: function (file, inputElem) {
