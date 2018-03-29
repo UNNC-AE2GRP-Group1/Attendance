f573e77ec552b734bf8333cc1f64bef09baee198
zy18701 <zy18701@nottingham.edu.cn>
2018-03-29 12:38:21 +0800

    delete module-key add comment of data needed in view.py

--- Attendance/statistics/urls.py
+++ Attendance/statistics/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path, re_path
+from session.views import module_attendance_history
 
 from . import views
 
 # todo: add url tests
 urlpatterns = [
-    path('students/(<int:student_id>/stat/', views.student_attendance_history),
-    path('modules/(<int:module_pk>/stat/', views.module_attendance_history),
+  path('statistics/', views.module_attendance_history, name='module_attendance_history')
+ #   path('students/(<int:student_id>/stat/', views.student_attendance_history),
+ #  path('modules/(<int:module_pk>/stat/', views.module_attendance_history),
 ]
--- Attendance/statistics/views.py
+++ Attendance/statistics/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render
 from django.http import Http404
 from student.models import Student
-from session.models import Module
+from session.models import Module, Session
+from bridgekeeper import perms
+#from .models import *
 
 # todo: which student should a user be allowed to view?
-def student_attendance_history(request, student_id):
-    try:
-        s = Student.objects.get(student_id=student_id)
-    except Student.DoesNotExist:
-        raise Http404("Student does not exist")
+def module_attendance_history(request):
+#    try:
+#       m = Module.objects.all()
+#       vm = filter(request.user,m)
+#       sessions = Session.objects.get(module=vm)
+#    except Module.DoesNotExist:
+#        raise Http404("Module does not exist")
     return render(
         request,
-        'statistics/student.html',
-        # {'poll': p}
+         'statistics/module.html'
+ #        {'sessions': sessions},{'modules': vm}
     )
 
-def module_attendance_history(request, module_pk):
-    try:
-        m = Module.objects.get(pk=module_pk)
-    except Module.DoesNotExist:
-        raise Http404("Module does not exist")
-    return render(
-        request,
-        'statistics/module.html',
-        # {'poll': p}
-    )
+
+#def module_attendance_history(request):
+#    try:
+# m = Module.objects.all()
+# vm = perms['module.view'].filter(request.user,m)
+# sessions = Session.objects.get(module=vm)
+#   except Module.DoesNotExist:
+#        raise Http404("Module does not exist")
+#    return render(
+ #       request,
+#        'statistics/module.html',
+ #       { 'sessions': sessions }
+#    )
