45219af284a20a8df9cf2ca61e9ed3f465daed1e
MarvinZeng <772400725@qq.com>
2018-04-01 17:19:24 +0800

    impl session creation

--- Attendance/session/forms.py
+++ Attendance/session/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.forms import ModelForm
 import datetime
 from .models import *
@@ -8,8 +7,7 @@ class ModuleCreateForm(ModelForm):
         model = Module
         exclude = ['students', 'attendance_rate']
 
-class SessionCreateForm(forms.Form):
-    time = forms.DateTimeField(initial=datetime.datetime.now)
-    repeat = forms.BooleanField()
-    repeat_time = forms.IntegerField(min_value=1)
-    type = forms.ChoiceField(choices=Session.SESSION_TYPES)
+class SessionCreateForm(ModelForm):
+    class Meta:
+        model = Session
+        fields = ['time', 'duration', 'place', 'type']
